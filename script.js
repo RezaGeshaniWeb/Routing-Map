f7ce695a778bc7cb154ff265f073720463acbe70
reza geshani <rezageshaniweb@gmail.com>
2025-12-24 15:49:06 +0330

    refactor: edit additional codes

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 // set tag id
 // set Set the latitude and longitude of the location
 // Setting the initial zoom level (1, 18)
+const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
 
-/*
-const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15, {
-  animate: true,
-  duration: 2,
-});
-*/
-
-const map = L.map("map", {
-  center: [35.70015230480259, 51.33811941768732],
-  zoom: 12,
-  //   minZoom: 5,
-  //   maxZoom: 13,
-  //   zoomControl: true,
-});
 
 // get api & set map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
 
 // set custom icon for pointer
 const metroIcon = L.icon({
   iconUrl: "https://www.svgrepo.com/show/397501/metro.svg",
   iconSize: [30, 30],
 });
+
 
 // add pointer
 let marker1 = L.marker([35.70008260317534, 51.33816233315887]).addTo(map);
@@ -33,6 +22,7 @@ let marker2 = L.marker([35.69969245203644, 51.32015210727593], {
   icon: metroIcon,
   title: "subway",
 }).addTo(map);
+
 
 // add popup
 // marker1.bindPopup('azadi tower')
