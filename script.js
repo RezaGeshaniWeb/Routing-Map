237830b877b8f7135ede0188eb3b47d7f20dfb2a
Reza Geshani <rezageshaniweb@gmail.com>
2025-12-28 21:28:01 +0330

    feat: add line between to location

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const map = L.map("map").setView([35.70015230480259, 51.33811941768732], 15);
+const map = L.map("map").setView([35.704065544779105, 51.10967164109585], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
@@ -10,8 +10,17 @@ const point2 = [35.707550374942485, 51.22794632644294];
 const marker1 = L.marker(point1).addTo(map);
 const marker2 = L.marker(point2).addTo(map);
 
-const url = `http://router.project-osrm.org/route/v1/driving/${point1[1]},${point1[0]};${point2[1], point2[0]}?overview=false`;
+const url = `http://router.project-osrm.org/route/v1/driving/${point1[1]},${point1[0]};${point2[1]},${point2[0]}?overview=full&geometries=geojson`;
 
 fetch(url)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log(data)
+    const coordinates = data.routes[0].geometry.coordinates
+    const latLng = coordinates.map(coor => [coor[1], coor[0]])
+    L.polyline(latLng, {
+      color: 'blue',
+      weight: 4,
+      opacity: 0.7,
+    }).addTo(map)
+  });
